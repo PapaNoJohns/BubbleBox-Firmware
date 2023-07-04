32126607db3aafcd68579b51b7b0f0a01719767f
PapaNoJohns <PapaNoJohns@gmail.com>
2023-07-03 22:29:58 -0700

    Added SF6 mode

---
+++ src/modes/SF6.cpp
@@ -0,0 +1,50 @@
+#include "modes/SF6.hpp"
+
+SF6::SF6(socd::SocdType socd_type) : ControllerMode(socd_type) {
+    _socd_pair_count = 1;
+    _socd_pairs = new socd::SocdPair[_socd_pair_count]{
+        socd::SocdPair{&InputState::left, &InputState::right},
+    };
+}
+
+void SF6::HandleSocd(InputState &inputs) {
+    if (inputs.down && (inputs.mod_x || inputs.c_up)) {
+        inputs.down = false;
+    }
+    InputMode::HandleSocd(inputs);
+}
+
+void SF6::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
+    // Directions
+    outputs.dpadLeft = inputs.left;
+    outputs.dpadRight = inputs.right;
+    outputs.dpadDown = inputs.down;
+    outputs.dpadUp = inputs.mod_x || inputs.mod_y;
+
+    // Menu keys
+    outputs.start = inputs.start;
+    outputs.select = inputs.c_left;
+    outputs.home = inputs.c_down;
+
+    // Right hand bottom row
+    outputs.a = inputs.b;
+    outputs.b = inputs.x;
+    outputs.triggerRDigital = inputs.z;
+
+    // Right hand top row
+    outputs.x = inputs.r;
+    outputs.y = inputs.y;
+    outputs.buttonR = inputs.lightshield;
+    outputs.buttonL = inputs.midshield;
+
+    // Drive Impact and parry
+    outputs.triggerLDigital = inputs.l;
+    outputs.buttonL = inputs.up;
+}
+
+void SF6::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
+    outputs.leftStickX = 128;
+    outputs.leftStickY = 128;
+    outputs.rightStickX = 128;
+    outputs.rightStickY = 128;
+}
